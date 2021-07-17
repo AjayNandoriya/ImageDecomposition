e267283623993183fa9530f905be4cc4cb8777f7
AjayNandoriya <cartoon.ajay@gmail.com>
2021-07-18 06:48:37 +0800

    add analyze_loss

--- scripts/sem.py
+++ scripts/sem.py
@@ -5,10 +5,11 @@ import tensorflow as tf
 import numpy as np
 
 def cmae(y_true, y_pred):
-    diff = tf.abs(y_true-y_pred)[:,15:-15,15:-15,:]
+    diff = tf.square(y_true-y_pred)[:,15:-15,15:-15,:]
     full_diff = tf.reduce_mean(diff)
-    clipped_diff = tf.reduce_mean(tf.clip_by_value(diff, clip_value_min=0.1, clip_value_max=100.0))
-    loss = full_diff + clipped_diff*10
+    clipped_diff = tf.clip_by_value(diff, clip_value_min=0.064, clip_value_max=100.0)
+    clipped_loss = tf.reduce_mean(clipped_diff)
+    loss = full_diff + clipped_loss*10
     return loss
 
 def create_model(N_scale=4, N_features=16):
@@ -17,8 +18,13 @@ def create_model(N_scale=4, N_features=16):
     inp = tf.keras.layers.Input(shape=(None,None,1))
     sections = tf.keras.layers.Conv2D(N_features, kernel_size=kernel_size, strides=strides, padding='same', activation='softmax', name='regions')(inp)
     sections = tf.keras.layers.UpSampling2D(size=strides,interpolation='bilinear')(sections)
-    features = tf.keras.layers.DepthwiseConv2D(kernel_size=kernel_size, padding='same', activation='relu')(sections)
+    features = tf.keras.layers.DepthwiseConv2D(kernel_size=kernel_size, padding='same', activation='tanh')(sections)
     out = tf.keras.layers.Conv2D(1, kernel_size=(1,1), padding='same', activation=None, name='weight_sum')(features)
+
+    base_kernel_size = [2**(N_scale+1), 2**(N_scale+1)] 
+    dout = tf.keras.layers.AveragePooling2D(pool_size=base_kernel_size, strides=base_kernel_size, padding='same')(inp)
+    base_out = tf.keras.layers.UpSampling2D(size=base_kernel_size,interpolation='bilinear')(dout)
+    out = tf.keras.layers.Add()([out, base_out])
     model = tf.keras.models.Model(inputs=[inp],outputs=[out])
     model.compile(loss=cmae, metrics='mse', loss_weights=50)
     return model
@@ -138,7 +144,7 @@ def analyse_model():
     diff_hist = np.histogram(np.abs(diff), bins=100)
 
     # base line
-    base_model = create_base_model()
+    base_model = create_base_model(5)
     
     base_out_img4d = base_model.predict(dg.img4d)
     base_out_img = base_out_img4d[0,:,:,0]
@@ -160,6 +166,29 @@ def analyse_model():
     plt.plot(diff_hist[1][:-1], diff_hist[0]),plt.grid(True)
     plt.show()
 
+def analyze_loss():
+    base_dir =os.path.dirname(__file__)
+    N_features = 8
+    model_fname = os.path.join(base_dir, f'sem_model_{N_features}.h5')
+    img_fname = os.path.join(base_dir,'..','data','sem_images','SRAM_22nm.jpg')
+    dg = DataGenerator(img_fname)
+
+    if not os.path.isfile(model_fname):
+      print(f'model not found:{model_fname}')
+      return
+
+    N_x = int(np.math.ceil(np.sqrt(N_features)))
+    
+    model = create_model(N_features=N_features)
+    model.load_weights(model_fname)
+    
+    out_img4d = model.predict(dg.img4d)
+    
+    loss = cmae(out_img4d, dg.img4d)
+    print(loss)
+    pass
+
 if __name__ == '__main__':
     # train()
     analyse_model()
+    # analyze_loss()
